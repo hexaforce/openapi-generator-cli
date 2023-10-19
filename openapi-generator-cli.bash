e2f36ff8427fb5bc4e44f1f212246af446105623
tantaka <tomokazu.tantaka@gmail.com>
2023-10-19 10:22:13 +0900

    ll

--- openapi-generator-cli.bash
+++ openapi-generator-cli.bash
@@ -1,34 +1,30 @@
-#!/bin/zsh -ex
+#!/bin/bash
+
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 CLI=openapitools/openapi-generator-cli
 
 # docker run $CLI help generate
 
-rm -rf mysql-schema
-rm -rf typescript-fetch
-rm -rf php-nextgen
+openapi-generator-cli() {
+ local generator=$1 
+ docker run --rm --volume `pwd`:/pwd $CLI generate \
+  --skip-operation-example --skip-validate-spec --http-user-agent curl/8.1.2 \
+  --package-name FpvJapan \
+  --input-spec /pwd/api.yaml \
+  --generator-name $generator \
+  --output /pwd/$generator
+}
 
-docker run --rm --volume `pwd`:/pwd $CLI generate \
- --skip-operation-example --skip-validate-spec --http-user-agent curl/8.1.2 \
- --package-name FpvJapan \
- --input-spec /pwd/api.yaml \
- --generator-name php-nextgen \
- --output /pwd/php-nextgen
+# openapi-generator-cli "php-laravel"
+# openapi-generator-cli "php-lumen"
+# openapi-generator-cli "php-mezzio-ph"
+openapi-generator-cli "php-slim4"
+# openapi-generator-cli "php-symfony"
+# openapi-generator-cli "php-nextgen"
 
-docker run --rm --volume `pwd`:/pwd $CLI generate \
- --skip-operation-example --skip-validate-spec --http-user-agent curl/8.1.2 \
- --package-name FpvJapan \
- --input-spec /pwd/api.yaml \
- --generator-name typescript-fetch \
- --output /pwd/typescript-fetch
-
-docker run --rm --volume `pwd`:/pwd $CLI generate \
- --skip-operation-example --skip-validate-spec --http-user-agent curl/8.1.2 \
- --package-name FpvJapan \
- --input-spec /pwd/api.yaml \
- --generator-name mysql-schema \
- --output /pwd/mysql-schema
+openapi-generator-cli "mysql-schema"
+openapi-generator-cli "typescript-fetch"
 
 find . -name .openapi-generator-ignore -type f | xargs rm
 
